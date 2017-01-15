2f910101b865b2cd7606e6bb1f7aa11b883b8161
tuutuu <tuure.piitulainen@gmail.com>
2017-01-15 23:40:58 +0200

    Pieni muutos testidataan

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -7,4 +7,4 @@ INSERT INTO Movie (nimi) VALUES ('Hymyilevä mies');
 INSERT INTO Movie (nimi) VALUES ('Evil Bong 420');
 INSERT INTO Review (arvostelija_id, elokuva_id, teksti, arvosana) VALUES (1, 2, 'Kaunis elokuva', 10.0);
 INSERT INTO Review (arvostelija_id, elokuva_id, teksti, arvosana) VALUES (1, 1, 'paras leffa oikeesti', 8.2);
-INSERT INTO Review (arvostelija_id, elokuva_id, teksti, arvosana) VALUES (1, 1, 'Aegisjwri eiojepa epijpti rtpjirwpi rpwiyjwp projrpow wporjt pejpo wprjpwr rwjpg wrpjowpro', 8.34896793487348976);
+INSERT INTO Review (arvostelija_id, elokuva_id, teksti, arvosana) VALUES (1, 1, 'Aegisjwri eiojepa epijpti rtpjirwpi rpwiyjwp projrpow wporjt pejpo wprjpwr rwjpg wrpjowpro', 8.3);
